3fae309d8c5222d156ad3ca9f792e2c1d382ae56
Rafaelp05 <pereiraborgesrafael05@gmail.com>
2025-07-23 18:48:55 -0300

    Show

--- src/main/java/com/controle/Main.java
+++ src/main/java/com/controle/Main.java
@@ -2,6 +2,4 @@ package com.controle;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+        System.out.println("Artur show")
