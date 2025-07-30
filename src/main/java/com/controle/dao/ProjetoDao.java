91a0637f387147a14700927eff1f83a02a53e7f2
Rafaelp05 <pereiraborgesrafael05@gmail.com>
2025-07-30 15:05:15 -0300

    feat: adicionar método criarProjeto na classe ProjetoDao para inserir projetos no banco de dados

--- src/main/java/com/controle/dao/ProjetoDao.java
+++ src/main/java/com/controle/dao/ProjetoDao.java
@@ -13,4 +13,21 @@ public class ProjetoDao {
     public ProjetoDao(Connection connection){
         this.connection = connection;
     }
+
+    public void criarProjeto(Projeto projeto){
+        String sql = "INSERT INTO projetos (nome, descricao, dataInicio, dataFim, status) VALUES (?, ?, ?, ?, ?)";
+
+        try {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, projeto.getNome());
+            statement.setString(2, projeto.getDescricao());
+            statement.setDate(3, Date.valueOf(projeto.getDataInicio()));
+            statement.setDate(4, Date.valueOf(projeto.getDataFim()));
+            statement.setString(5, projeto.getStatus());
+            statement.executeUpdate();
+            System.out.println("Projeto adicionado com sucesso!");
+        }catch (SQLException e){
+            System.out.println("Erro ao adicionar projeto: " + e.getMessage());
+        }
+    }
 }
