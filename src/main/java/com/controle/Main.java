9473999957305b6e04f38d1fc31375796c86fb3c
Rafaelp05 <pereiraborgesrafael05@gmail.com>
2025-07-30 15:20:42 -0300

    feat: adicionar lógica para criar um projeto no método main da classe Main

--- src/main/java/com/controle/Main.java
+++ src/main/java/com/controle/Main.java
@@ -1,7 +1,12 @@
 package com.controle;
 
 import com.controle.dao.Conexao;
+import com.controle.dao.ProjetoDao;
+import com.controle.model.Projeto;
+
 import java.sql.Connection;
+import java.sql.Date;
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,8 +15,14 @@ public class Main {
         // Testando conexão com o banco de dados
         Conexao conexao = new Conexao();
         Connection conn = conexao.conectarBanco();
-        
-        // Fechando a conexão se ela foi estabelecida
+
+        LocalDate hoje = LocalDate.now();
+        Projeto projeto = new Projeto("Rafael", "Descricao", hoje, hoje.plusDays(30));
+
+        ProjetoDao projetoDao = new ProjetoDao(conn);
+
+        projetoDao.criarProjeto(projeto);
+
         if (conn != null) {
             try {
                 conn.close();
