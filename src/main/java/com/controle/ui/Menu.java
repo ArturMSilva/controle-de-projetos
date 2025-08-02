41e3a5f5be63e078c4db6ef8b0b27aad7b1ee591
Rafaelp05 <pereiraborgesrafael05@gmail.com>
2025-08-02 09:52:13 -0300

    feat: adicionar lógica para criar um projeto no menu interativo

--- src/main/java/com/controle/ui/Menu.java
+++ src/main/java/com/controle/ui/Menu.java
@@ -1,9 +1,18 @@
 package com.controle.ui;
 
+import com.controle.dao.Conexao;
+import com.controle.dao.ProjetoDao;
+import com.controle.model.Projeto;
+
+import java.sql.Connection;
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Menu {
     Scanner scanner = new Scanner(System.in);
+
+    Conexao conexao = new Conexao();
+    Connection connection = conexao.conectarBanco();
 
     public void menu(){
         System.out.println("1. Projetos \n2. Tarefas");
@@ -16,8 +25,22 @@ public class Menu {
                 System.out.println("Escolha uma opção: ");
                 int opcao = scanner.nextInt();
 
+                scanner.nextLine();
                 switch (opcao){
+                    case 1:
+                        System.out.println("Digite o nome do projeto: ");
+                        String nome = scanner.nextLine();
 
+                        System.out.println("Digite uma descrição para o projeto: ");
+                        String descricao = scanner.nextLine();
+
+                        LocalDate dataInicio = LocalDate.now();
+
+                        LocalDate dataFim = dataInicio.plusDays(30);
+
+                        Projeto projeto = new Projeto(nome, descricao, dataInicio, dataFim);
+                        ProjetoDao projetoDao = new ProjetoDao(connection);
+                        projetoDao.criarProjeto(projeto);
                 }
                 break;
 
